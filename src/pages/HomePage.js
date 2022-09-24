4543a018c3a8e014792d8215e48cf893530b292b
mahendra <sawwaserem@gmail.com>
2022-09-24 11:14:06 +0530

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+
+
+
+import React from "react";
+import BannerComponent from "../components/banner/BannerComponent"
+import CoinTableComponent from "../components/CoinTableComponent";
+
+const Homepage = () => {
+  return (
+    <>
+      <BannerComponent />
+      <CoinTableComponent />
+    </>
+  );
+};
+
+export default Homepage;
